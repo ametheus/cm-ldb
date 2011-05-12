8430c840ce41ce3fa1fb5c9cee96f30c4738fe16
Thijs van Dijk <thijs@nepomuck>
2011-05-12 22:16:40 +0200

    Eerste indexpagina.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Content-type: text/plain");
+
+# Als je dit kunt lezen, ben ik vergeten libapache2-php5 te installeren. Oeps.
+
+?>Als je dit kunt lezen, doet hij het nog niet. Maar wel bijna.
