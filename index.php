d26f7bfef7741180918bc87d7159db3af2715d46
Thijs van Dijk <thijs@nepomuck>
2011-05-14 20:18:17 +0200

    Lijst van queries.

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
 
 require_once( "inc/db.inc" );
+require_once( "inc/query.inc" );
 
 header("Content-type: text/plain");
 
 # Als je dit kunt lezen, ben ik vergeten libapache2-php5 te installeren. Oeps.
 
 
-
-$Q1 = <<<EOT
-    SELECT Voornaam, Tussenvoegsel, Achternaam
-    FROM persoon
-    WHERE pers_id = @`per-soon`
-EOT;
-$Q2 = <<<EOT
-    SELECT Klasse, Groepsnaam, van, tot
-    FROM lidVan
-        JOIN groep USING ( groep_id )
-    WHERE pers_id = @`per-soon`
-EOT;
-$Query = new Command( array($Q1,$Q2), DB::ro() );
-print_r( $Query->execute( array( 'per-soon' => 2021 ) ) );
+$qs = Query::queries_in_dir( Config::$query_dir );
+foreach ( $qs as $q )
+{
+    print( $q->Title . "\n" );
+}
 
 
 
