6a030a6cb44fa898ef31e7daa4dc4e1edc6a60ab
Thijs van Dijk <thijs@nepomuck>
2011-05-14 23:43:38 +0200

    Tabbladen

--- exec-query.php
+++ exec-query.php
@@ -10,5 +10,47 @@ if ( ! $Q )
     die( "Query niet gevonden." );
 }
 
-print_r( $Q->execute() );
+$Result = $Q->execute();
 
+
+
+
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title><?=$Q->Title?></title>
+        
+        <link type="text/css" href="/css/humanity/jquery-ui-1.8.12.custom.css" rel="stylesheet" />	
+		<script type="text/javascript" src="/js/jquery-1.5.1.min.js"></script>
+		<script type="text/javascript" src="/js/jquery-ui-1.8.12.custom.min.js"></script>
+        
+        <script type="text/javascript">
+        $(function(){
+            $("#Table-tabs").tabs();
+        })
+        </script>
+    </head>
+    <body>
+        <div id="Table-tabs">
+            <ul>
+<?php
+foreach ( array_keys($Result) as $table )
+{
+    $tid = preg_replace('/[^a-zA-Z0-9]/','',$table);
+    print( "                <li><a href=\"#table-$tid\">$table</a></li>\n" );
+}
+?>
+            </ul>
+<?php
+foreach ( $Result as $table=>$data )
+{
+    $tid = preg_replace('/[^a-zA-Z0-9]/','',$table);
+    print( "            <div id=\"table-$tid\">\n" );
+    print_r( $data );
+    print( "            </div>\n" );
+}
+?>
+        </div>
+    </body>
+</html>
