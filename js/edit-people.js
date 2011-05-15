43382465f19e327795f5761929f5b4f71df59000
Thijs van Dijk <thijs@nepomuck>
2011-05-15 18:00:11 +0200

    Details laten zien.

--- js/edit-people.js
+++ js/edit-people.js
@@ -1,9 +1,11 @@
 
 var selectedIndex = 0;
 var IDs = null;
+var Details = {};
 
 $(function()
 {
+    $('#EditPeople').addClass('loading');
     $.ajax({
         url: '/bewerken/json/IDs',
         dataType: 'json',
@@ -36,6 +38,7 @@ function select_index( index )
     if ( index < 0 ) { index = 0; }
     
     selectedIndex = index;
+    select_this();
 }
 
 function select_id( pers_id )
@@ -57,6 +60,103 @@ function select_this()
 {
     location.hash = pid();
     $('#IDNavigator input').val(pid());
+    
+    if ( pid() in Details )
+    {
+        $('#EditPeople').removeClass('loading');
+        update_fields();
+    }
+    else
+    {
+        $('#EditPeople').addClass('loading');
+        load_details( pid() );
+    }
+}
+
+function load_details( pers_id )
+{
+    $.ajax({
+        type: 'POST',
+        url: '/bewerken/json/details',
+        data: {'pers_id': pers_id},
+        success: function(data)
+        {
+            //var pid;
+            var update = !( pid() in Details );
+            for ( var i = 0; i < data.length; i++ )
+            {
+                r = data[i];
+                p = r['pers_id'];
+                if ( ! ( p in Details ))
+                {
+                    Details[p] = r;
+                }
+            }
+            if (( update ) && ( pid() in Details ))
+            {
+                update_fields();
+            }
+        },
+        dataType: 'json'
+    });
+}
+
+function update_fields()
+{
+    if ( ! ( pid() in Details )) { alert("not anymore."); return; }
+    var d = Details[pid()];
+    
+    $('#Field-Achternaam')       .val(d['achternaam']);
+    $('#Field-Aangetrouwdenaam') .val(d['aangetrouwdenaam']);
+    $('#Field-Voorletters')      .val(d['voorletters']);
+    $('#Field-Voornaam')         .val(d['voornaam']);
+    $('#Field-Tussenvoegsel')    .val(d['tussenvoegsel']);
+    $('#Field-Titel')            .val(d['titel']);
+    $('#Field-Geslacht')         .val(d['geslacht']);
+    $('#Field-Adres')            .val(d['adres']);
+    $('#Field-Postcode')         .val(d['postcode']);
+    $('#Field-Plaats')           .val(d['plaats']);
+    $('#Field-Land')             .val(d['land']);
+    $('#Field-Post')             .val(d['post']);
+    $('#Field-Telefoon')         .val(d['telefoon']);
+    $('#Field-Mobiel')           .val(d['mobiel']);
+    $('#Field-Email')            .val(d['email']);
+    $('#Field-Geboortedatum')    .val(d['geboortedatum']);
+    $('#Field-Sterftedatum')     .val(d['sterftedatum']);
+    $('#Field-Banknummer')       .val(d['banknummer']);
+    $('#Field-Tenaamstelling')   .val(d['tenaamstelling']);
+    $('#Field-Nationaliteit')    .val(d['nationaliteit']);
+    $('#Field-Voorkeurstaal')    .val(d['voorkeurstaal']);
+    $('#Field-Opm')              .val(d['opm']);
+    
+    $('#EditPeople').removeClass('loading');
+}
+
+function update_details()
+{
+    var d = {};
+    d['achternaam']         = $('#Field-Achternaam').val();
+    d['aangetrouwdenaam']   = $('#Field-Aangetrouwdenaam').val();
+    d['voorletters']        = $('#Field-Voorletters').val();
+    d['voornaam']           = $('#Field-Voornaam').val();
+    d['tussenvoegsel']      = $('#Field-Tussenvoegsel').val();
+    d['titel']              = $('#Field-Titel').val();
+    d['geslacht']           = $('#Field-Geslacht').val();
+    d['adres']              = $('#Field-Adres').val();
+    d['postcode']           = $('#Field-Postcode').val();
+    d['plaats']             = $('#Field-Plaats').val();
+    d['land']               = $('#Field-Land').val();
+    d['post']               = $('#Field-Post').val();
+    d['telefoon']           = $('#Field-Telefoon').val();
+    d['mobiel']             = $('#Field-Mobiel').val();
+    d['email']              = $('#Field-Email').val();
+    d['geboortedatum']      = $('#Field-Geboortedatum').val();
+    d['sterftedatum']       = $('#Field-Sterftedatum').val();
+    d['banknummer']         = $('#Field-Banknummer').val();
+    d['tenaamstelling']     = $('#Field-Tenaamstelling').val();
+    d['nationaliteit']      = $('#Field-Nationaliteit').val();
+    d['voorkeurstaal']      = $('#Field-Voorkeurstaal').val();
+    d['opm']                = $('#Field-Opm').val();
 }
 
 
