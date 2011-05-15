e5059488357925b733057898986eb6a54ac22031
Thijs van Dijk <thijs@nepomuck>
2011-05-15 14:44:11 +0200

    Permalinks naar leden zijn nu mogelijk.

--- js/edit-people.js
+++ js/edit-people.js
@@ -1,4 +1,5 @@
 
+var selectedIndex = 0;
 var IDs = null;
 
 $(function()
@@ -9,10 +10,53 @@ $(function()
         success: function(data)
         {
             IDs = data;
-            location.hash = IDs[IDs.length-1];
+            if ( location.hash.length > 1 )
+            {
+                select_id( location.hash.substr(1) );
+            }
+            else
+            {
+                select_index( IDs.length-1 );
+            }
         }
     });
 })
 
+
+function pid()
+{
+    return IDs[selectedIndex];
+}
+
+function select_index( index )
+{
+    if ( ! IDs ) { return; }
+    if ( IDs.length == 0 ) { return; }
+    if ( index >= IDs.length ) { index = IDs.length - 1; }
+    if ( index < 0 ) { index = 0; }
+    
+    selectedIndex = index;
+}
+
+function select_id( pers_id )
+{
+    if ( ! IDs ) { return; }
+    for ( var i = 0; i < IDs.length; i++ )
+    {
+        if ( IDs[i] == pers_id )
+        {
+            selectedIndex = i;
+            select_this();
+            return;
+        }
+    }
+    select_index( IDs.length-1 );
+}
+
+function select_this()
+{
+    location.hash = pid();
+    $('#IDNavigator input').val(pid());
+}
 
 
