07576049cfa11e4b61ea75c74ee047376fcdda7b
Thijs van Dijk <thijs@nepomuck>
2011-05-15 16:20:37 +0200

    Detail-adapter.

--- edit-people.php
+++ edit-people.php
@@ -16,9 +16,10 @@ if ( ! isset($_GET["json"]) )
 }
 
 
+header( "Content-type: text/plain" );
+
 if ( $_GET["json"] == "IDs" )
 {
-    header( "Content-type: text-plain" );
     $PA = Adapters\Persoon::IDs();
     $IDs = $PA->execute(array());
     print( "[" );
@@ -28,4 +29,14 @@ if ( $_GET["json"] == "IDs" )
         print $r["pers_id"];
     }
     print( "]" );
+}
+elseif (( $_GET["json"] == "details" ) && ( is_numeric(@$_REQUEST["pers_id"]) ))
+{
+    $Detail = Adapters\Persoon::Detail();
+    $Ds = $Detail->execute(array('persoon'=>$_REQUEST["pers_id"]));
+    print(json_encode($Ds));
+}
+else
+{
+    print "null";
 }
