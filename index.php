8ed6dbaa9e1991eec6fdea43d05836b1a92e0122
Thijs van Dijk <thijs@nepomuck>
2011-05-18 18:31:22 +0200

    Begonnen aan versie 3.2.2

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ function LoQ()
 ?><!DOCTYPE html>
 <html>
     <head>
-        <title>Ledendatabase v3.2.1</title>
+        <title>Ledendatabase v3.2.2</title>
         
         <link rel="stylesheet" href="/css/common.css" />
         <link rel="stylesheet" href="/css/query-list.css" />
